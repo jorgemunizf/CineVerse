ee9cfd9a164fad147adc56139696b3121683a9bf
Jorge Filho <jorge.munizfilho@gmail.com>
2025-12-11 19:11:03 -0300

    feat: add synopsis movie

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,5 @@
 const API_URL = `${CONFIG.api_base_url}/discover/movie?sort_by=popularity.desc&api_key=${CONFIG.API_KEY}&language=pt-BR`;
-const SEARCH_API = `${CONFIG.api_base_url}/search/movie?api_key=${CONFIG.API_KEY}&query="`;
+const SEARCH_API = `${CONFIG.api_base_url}/search/movie?api_key=${CONFIG.API_KEY}&language=pt-BR&query=`;
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
@@ -10,19 +10,10 @@ const getMovies = async (url) => {
     const res = await fetch(url);
     const data = await res.json();
 
-    console.log("4. Resposta COMPLETA da API: ", data);
-
-    if (data.results) {
+    if (data.results && data.results.length > 0) {
       showMovies(data.results);
     } else {
-      // Se entrar aqui, é porque a API deu erro (ex: chave inválida)
-      console.error("ERRO DA API:", data);
-
-      // Mostra o erro na tela para você saber o que houve
-      const errorMsg = data.status_message || "Erro desconhecido na API";
-      document.getElementById(
-        "main"
-      ).innerHTML = `<h2 style="color:red; text-align:center">Erro da API: ${errorMsg}</h2>`;
+      main.innerHTML = `<h2 style="text-align:center; width:100%">Nenhum filme encontrado</h2>`;
     }
   } catch (error) {
     console.error("Erro ao buscar filme: ", error);
@@ -46,8 +37,12 @@ const showMovies = (movie) => {
         <div class="movie-info">
             <h3>${title}</h3>
             <span class="${getClassByRate(vote_average)}">
-            ${vote_average}
+            ${vote_average.toFixed(1)}
             </span>
+        </div>
+        <div class="overview">
+            <h3>Sinopse</h3>
+            ${overview || "Sinopse indisponível"}
         </div>
     </a>
     `;
@@ -71,6 +66,8 @@ form.addEventListener("submit", (e) => {
     getMovies(SEARCH_API + searchTerm);
     search.value = "";
   } else {
-    window.location.reload;
+    window.location.reload();
   }
 });
+
+getMovies(API_URL);
